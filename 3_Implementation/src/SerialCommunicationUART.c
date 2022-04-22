b4087cd624952959fd9613d73a7d23c451823fa5
patilsliet <101458188+patilsliet@users.noreply.github.com>
2022-04-22 16:38:16 +0530

    Update SerialCommunicationUART.c

--- 3_Implementation/src/SerialCommunicationUART.c
+++ 3_Implementation/src/SerialCommunicationUART.c
@@ -1,6 +1,6 @@
 /**
  * @file SerialCommunicationUART.c
- * @author Gnaneshwaran B (gnanesh05032001@gmail.com)
+ * @author Amitkumar Patil(1840153@sliet.ac.in)
  * @brief A file where serial communication functions are implemented
  * @version 0.1
  * @date 2022-04-20
