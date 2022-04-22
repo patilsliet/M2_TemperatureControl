8559eff38d3b4dbc3697f7532ec4931b932d01c2
patilsliet <101458188+patilsliet@users.noreply.github.com>
2022-04-22 16:37:54 +0530

    Update ButtonSensorHeaterLED.c

--- 3_Implementation/src/ButtonSensorHeaterLED.c
+++ 3_Implementation/src/ButtonSensorHeaterLED.c
@@ -1,6 +1,6 @@
 /**
  * @file ButtonSensorHeaterLED.c
- * @author Gnaneshwaran B (gnanesh05032001@gmail.com)
+ * @author Amitkumar Patil(1840153@sliet.ac.in)
  * @brief A file where the functions for button and heater are implememted
  * @version 0.1
  * @date 2022-04-20
